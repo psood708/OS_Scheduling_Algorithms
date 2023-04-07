2f144c95b6034d0f0b05c4cd854748df0c218894
Manav Surti <93250855+infosecmanav@users.noreply.github.com>
2023-04-07 09:39:49 +0530

    Update RoundRobin.jsx

--- src/Pages/RoundRobin.jsx
+++ src/Pages/RoundRobin.jsx
@@ -23,15 +23,14 @@ Put all processes that are ready to run into the ready queue. Define a time slic
 The Round Robin algorithm ensures that all running processes are given the same amount of time on the CPU. In addition, the algorithm ensures that no single process will ever have exclusive access to the central processing unit (CPU) for too long.
 </li>
             <li className='mt-6'>Round Robin Algorithm Benefits: The algorithm guarantees that no programme is given preferential treatment and that all receive the same amount of processing time.
-<li>The algorithm ensures that no single task will consume excessive CPU time. This suggests that quick execution times are achieved for processes with low execution times.</li>
-<li>The algorithm allows for preemption, which means that one process can be replaced by another in the middle of its execution. This avoids situations where one process becomes stuck and impedes another.</li>
-</li>
+<li>The algorithm ensures that no single task will consume excessive CPU time.</li>
             <li className='mt-6'>Some drawbacks of the Round Robin algorithm are as follows: The algorithm has a high overhead because of the need to manage a ready queue and separate time into discrete chunks. Especially when dealing with a large number of processes, this can cause overall performance to suffer. When there are many concurrently running processes, the algorithm may not function optimally. This is because there is a limited amount of time that a process can use the CPU before being preempted, which can lead to a lot of context switching and overhead. The algorithm's efficiency relies heavily on the time quantum used. There will be excessive context switching if the time quantum is too small, while prolonged CPU monopolisation by processes is possible if the time quantum is too large</li>
           </ul>
         </div>
         </div>
         {/* direct to simulator */}
-      <button className='p-10 text-black border-2 border-black hover:border-white bg-red-500 hover:bg-red-700 duration-700 hover:text-white rounded-3xl text-[20px] left-3/4 relative mt-4 items-center '>Go to Simulation</button>
+      <button className='p-10 text-black border-2 border-black hover:border-white bg-red-500 hover:bg-red-700 duration-700 hover:text-white rounded-3xl text-[20px] left-3/4 relative mt-4 items-center '><a href="https://127.0.0.1/">Go to Simulation</a>
+</button>
 
 
 
