b426485ac4e68981535a4590e8068394f048c3b2
Parth <parth.sce21@sot.pdpu.ac.in>
2023-04-07 10:20:31 +0530

    merged

--- src/Pages/RoundRobin.jsx
+++ src/Pages/RoundRobin.jsx
@@ -1,51 +1,51 @@
 import React from 'react'
-import img  from "../assets/images/rr.jpg"
+import img from "../assets/images/rr.jpg"
 import { Navbar } from '../Components/Navbar'
 import { Link } from 'react-router-dom'
 export const RoundRobin = () => {
-  
+
   return (
     <div className='font-Gloock bg-[#19191c] text-white pb-10'>
-      <Navbar/>
-      
+      <Navbar />
+
       <div className='heading p-10 text-center hover:text-red-600 duration-500 text-[50px]'>
-     Round Robin Algorithm
+        Round Robin Algorithm
       </div>
       {/* //we need to flex this */}
       <div className='flex gap-10'>
         <img src={img} alt="roundrobin" className='w-5/12 rounded-xl ml-10'></img>
-        <div> 
+        <div>
           <ul className='list-disc p-8 text-[20px]'>
-            <li> Procedure of the Round Robin Algorithm
+            <li> Procedure of the Round Robin Algorithm: </li>
 
-<li>The Round Robin algorithm is widely used in operating systems for scheduling central processing units. The goal of the algorithm is to fairly and equitably distribute CPU time among running processes.</li> <li> The Round Robin algorithm consists of the following operations:
-Put all processes that are ready to run into the ready queue. Define a time slice, also called a time quantum, which is the most processing time a task can use before being preempted.</li> For the given time slice, run the first process in the ready queue.
-<li>If the process completes its execution within the time slice, it is taken out of the ready queue. The process is preempted and pushed to the end of the ready queue if it does not finish executing within the time slice.<li> It is necessary to repeat steps 3-5 until all processes in the ready queue have completed their execution.
-The Round Robin algorithm ensures that all running processes are given the same amount of time on the CPU. In addition, the algorithm ensures that no single process will ever have exclusive access to the central processing unit (CPU) for too long.
-</li>
-            <li className='mt-6'>Round Robin Algorithm Benefits: The algorithm guarantees that no programme is given preferential treatment and that all receive the same amount of processing time.
-<li>The algorithm ensures that no single task will consume excessive CPU time.</li>
-            <li className='mt-6'>Some drawbacks of the Round Robin algorithm are as follows: The algorithm has a high overhead because of the need to manage a ready queue and separate time into discrete chunks. Especially when dealing with a large number of processes, this can cause overall performance to suffer. When there are many concurrently running processes, the algorithm may not function optimally. This is because there is a limited amount of time that a process can use the CPU before being preempted, which can lead to a lot of context switching and overhead. The algorithm's efficiency relies heavily on the time quantum used. There will be excessive context switching if the time quantum is too small, while prolonged CPU monopolisation by processes is possible if the time quantum is too large</li>
-          </ul>
+              <li>The Round Robin algorithm is widely used in operating systems for scheduling central processing units. The goal of the algorithm is to fairly and equitably distribute CPU time among running processes.</li>
+              <li> The Round Robin algorithm consists of the following operations:
+                Put all processes that are ready to run into the ready queue. Define a time slice, also called a time quantum, which is the most processing time a task can use before being preempted.</li> For the given time slice, run the first process in the ready queue.
+              <li>If the process completes its execution within the time slice, it is taken out of the ready queue. The process is preempted and pushed to the end of the ready queue if it does not finish executing within the time slice.<li> It is necessary to repeat steps 3-5 until all processes in the ready queue have completed their execution.
+                The Round Robin algorithm ensures that all running processes are given the same amount of time on the CPU. In addition, the algorithm ensures that no single process will ever have exclusive access to the central processing unit (CPU) for too long.
+              </li>
+                <li className='mt-6'>Round Robin Algorithm Benefits: The algorithm guarantees that no programme is given preferential treatment and that all receive the same amount of processing time.</li>
+                  <li>The algorithm ensures that no single task will consume excessive CPU time.</li>
+                  <li className='mt-6'>Some drawbacks of the Round Robin algorithm are as follows: The algorithm has a high overhead because of the need to manage a ready queue and separate time into discrete chunks. Especially when dealing with a large number of processes, this can cause overall performance to suffer. When there are many concurrently running processes, the algorithm may not function optimally. This is because there is a limited amount of time that a process can use the CPU before being preempted, which can lead to a lot of context switching and overhead. The algorithm's efficiency relies heavily on the time quantum used. There will be excessive context switching if the time quantum is too small, while prolonged CPU monopolisation by processes is possible if the time quantum is too large</li>
+                </li>
+                </ul>
+              </div>
+            </div>
+            {/* direct to simulator */}
+
+            <button className='p-10 text-black border-2 border-black hover:border-white bg-red-500 hover:bg-red-700 duration-700 hover:text-white rounded-3xl text-[20px] left-3/4 relative mt-4 items-center '><Link to="/roundrobins">Go to Simulation</Link></button>
+
+
+
+
+
+
+
+
+
+
+
         </div>
-        </div>
-        {/* direct to simulator */}
-{/* <<<<<<< HEAD */}
-      <button className='p-10 text-black border-2 border-black hover:border-white bg-red-500 hover:bg-red-700 duration-700 hover:text-white rounded-3xl text-[20px] left-3/4 relative mt-4 items-center '><Link to="/roundrobins">Go to Simulation</Link></button>
-{/* ======= */}
-      {/* <button className='p-10 text-black border-2 border-black hover:border-white bg-red-500 hover:bg-red-700 duration-700 hover:text-white rounded-3xl text-[20px] left-3/4 relative mt-4 items-center '><a href="https://127.0.0.1/">Go to Simulation</a> */}
-{/* </button> */}
-{/* >>>>>>> 2f144c95b6034d0f0b05c4cd854748df0c218894 */}
 
-
-
-
-
-
-
-
-    </div>
-      </div>
-      </div>
-  )
+        )
 }
